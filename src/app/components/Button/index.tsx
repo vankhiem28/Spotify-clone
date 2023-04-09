e02a54c22788cdf95a5bb1ee75650fb0220e80ec
Vankhiem <vankhiem@gmail.com>
2023-04-09 12:15:46 +0700

    update file

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -14,7 +14,9 @@ function Button({ leftIcon, boxIcon, boxIconClass, overlay, onClick, children }:
     <div className="group flex items-center gap-4 py-2 px-4 rounded cursor-pointer">
       {leftIcon ? <span>{leftIcon}</span> : ""}
       {boxIcon ? (
-        <span className={`w-6 h-6 bg-primary rounded-sm flex items-center justify-center relative ${boxIconClass}`}>
+        <span
+          className={`w-6 h-6 bg-primary rounded-sm flex items-center justify-center relative ${boxIconClass}`}
+        >
           <span className="z-50">{boxIcon}</span>
           {overlay ? (
             <span className="before:absolute before:w-full before:h-full before:top-0 before:left-0 before:content-'' before:bg-overlay before:group-hover:bg-inherit before:transition-all before:duration-300"></span>
--- src/app/components/Center/components/Header/index.tsx
+++ src/app/components/Center/components/Header/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { NextIcon, PrevIcon } from "~/app/assests/img";
+import Button from "~/app/components/Button";
 
 function Header() {
   return (
-    <div className="w-full">
-      <button className="">Sign Up</button>
-      <button className="">Login</button>
+    <div className="w-full h-[64px] bg-overlay flex items-center justify-between px-8">
+      <div className="flex gap-4">
+        <button className="flex items-center justify-center w-8 h-8 rounded-full bg-black">
+          {<PrevIcon className="text-primary"></PrevIcon>}
+        </button>
+        <button className="flex items-center justify-center w-8 h-8 rounded-full bg-black">
+          {<NextIcon className="text-primary"></NextIcon>}
+        </button>
+      </div>
+      <div className="">
+        <button className="text-base text-primary font-bold font-circular-sp pr-8 hover:scale-105 hover:text-white">
+          Sign up
+        </button>
+        <button className="px-8 py-3 rounded-full bg-white text-base text-black font-bold font-circular-sp hover:scale-105">
+          Log in
+        </button>
+      </div>
     </div>
   );
 }
